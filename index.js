bf2b91f1b362c621ea73abc5d21144bb7b9ca73b
MustafaAbban <mustafaabban@utexas.edu>
2016-12-20 22:27:02 -0600

    added dependency to package

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var app = require('express')();
+var io = require('socket.io')(http);
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+
+io.on('connection', function(socket){
+  console.log('A user has connected');
+})
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
